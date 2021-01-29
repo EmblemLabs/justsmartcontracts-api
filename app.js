a7e7d76b34d6244b1610292e8e6110844859548a
shannon code <shannoncode@MacBook-Pro-2.local>
2021-01-29 11:52:03 -0500

    make it go

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'pug');
 
 app.use(cors());
